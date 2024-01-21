c20d9fc326dc762aa8bebb2c572324db4f998c0a
JaredFromSolway <157259403+JaredFromSolway@users.noreply.github.com>
2024-01-21 04:48:21 -0800

    Add files via upload

---
+++ _next/chucks/pages/index-f1fcaef6025d7b49.js
@@ -0,0 +1,42 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[405], {
+    73639: function(n, e, t) {
+        "use strict";
+        t.r(e);
+        var r = t(9008)
+          , u = t(85911)
+          , i = t(85893);
+        e.default = function(n) {
+            return (0,
+            i.jsxs)("div", {
+                children: [(0,
+                i.jsxs)(r.default, {
+                    children: [(0,
+                    i.jsx)("title", {
+                        children: "SPL-Token-Creator"
+                    }), (0,
+                    i.jsx)("meta", {
+                        name: "description",
+                        content: "SPL Token Creator"
+                    })]
+                }), (0,
+                i.jsx)(u.ug, {})]
+            })
+        }
+    },
+    45301: function(n, e, t) {
+        (window.__NEXT_P = window.__NEXT_P || []).push(["/", function() {
+            return t(73639)
+        }
+        ])
+    }
+}, function(n) {
+    n.O(0, [847, 249, 13, 774, 888, 179], (function() {
+        return e = 45301,
+        n(n.s = e);
+        var e
+    }
+    ));
+    var e = n.O();
+    _N_E = e
+}
+]);
